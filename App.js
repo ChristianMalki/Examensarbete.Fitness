9581a6ce21bff2fd0716fbe256b4c595d7ffe2d7
Christian Malki <christianmalki00@hotmail.com>
2023-08-14 11:41:59 +0200

    Test building

--- App.js
+++ App.js
@@ -4,35 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 import { FitnessContext } from "./Context";
 import HomeScreen from "./screens/HomeScreen";
 import StackNavigator from "./StackNavigator";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBddpYylg0Fv18sFjTEmLFWOAwF3mz0saM",
-  authDomain: "examensarbete-traningsap-52386.firebaseapp.com",
-  projectId: "examensarbete-traningsap-52386",
-  storageBucket: "examensarbete-traningsap-52386.appspot.com",
-  messagingSenderId: "498499143769",
-  appId: "1:498499143769:web:3be4cfaec11c2906a80948",
-  measurementId: "G-K7Y073R1PN",
-  //...
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 export default function App() {
-  /*   var docRef = db.collection("Fitness").doc("KOIsJeJEY3j77sX6ti5W"); */
 
-  async function getWorkouts(db) {
-    const citiesCol = collection(db, "Fitness");
-    const citySnapshot = await getDocs(citiesCol);
-    const cityList = citySnapshot.docs.map((doc) => doc.data());
-    console.log(cityList);
-    return cityList;
-  }
-  useEffect(() => {
-    getWorkouts(db);
-  }, []);
 
   const FitnessCards = async () => {
     try {
--- components/FitnessCards.js
+++ components/FitnessCards.js
@@ -1,55 +1,94 @@
 import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import React from "react";
+import { useEffect, useState } from "react";
 import fitness from "../data/fitness";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "firebase/firestore/lite";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBddpYylg0Fv18sFjTEmLFWOAwF3mz0saM",
+  authDomain: "examensarbete-traningsap-52386.firebaseapp.com",
+  projectId: "examensarbete-traningsap-52386",
+  storageBucket: "examensarbete-traningsap-52386.appspot.com",
+  messagingSenderId: "498499143769",
+  appId: "1:498499143769:web:3be4cfaec11c2906a80948",
+  measurementId: "G-K7Y073R1PN",
+  //...
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const FitnessCards = () => {
-  const FitnessData = fitness;
+  const [workouts, setWorkouts] = useState([]);
+
+  async function getWorkouts(db) {
+    const fit = collection(db, "fitness");
+    const fitSnapshot = await getDocs(fit);
+    const fitList = fitSnapshot.docs.map((doc) => doc.data());
+
+    setWorkouts(fitList[0].fitness);
+  }
+  useEffect(() => {
+    getWorkouts(db);
+  }, []);
+
   const navigation = useNavigation();
   return (
     <View>
-      {FitnessData.map((item, key) => (
-        <Pressable
-          onPress={() =>
-            navigation.navigate("Workout", {
-              image: item.image,
-              excersises: item.excersises,
-              id: item.id,
-            })
-          }
-          style={{ alignItems: "center", justifyContent: "center", margin: 10 }}
-          key={key}
-        >
-          <Image
-            style={{ width: "95%", height: 140, borderRadius: 7 }}
-            source={{ uri: item.image }}
-          />
-          <Text
+      {workouts.length !== 0 &&
+        workouts.map((item, key) => (
+          <Pressable
+            onPress={() =>
+              navigation.navigate("Workout", {
+                image: item.image,
+                excersises: item.excersises,
+                id: item.id,
+              })
+            }
             style={{
-              position: "absolute",
-              color: "white",
-              fontSize: 16,
-              fontWeight: "bold",
-              left: 20,
-              top: 20,
+              alignItems: "center",
+              justifyContent: "center",
+              margin: 10,
             }}
+            key={key}
           >
-            {item.name}
-          </Text>
-          <MaterialCommunityIcons
-            style={{
-              position: "absolute",
-              color: "white",
-              bottom: 15,
-              left: 20,
-            }}
-            name="lightning-bolt"
-            size={24}
-            color="black"
-          />
-        </Pressable>
-      ))}
+            <Image
+              style={{ width: "95%", height: 140, borderRadius: 7 }}
+              source={{ uri: item.image }}
+            />
+            <Text
+              style={{
+                position: "absolute",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+                left: 20,
+                top: 20,
+              }}
+            >
+              {item.name}
+            </Text>
+            <MaterialCommunityIcons
+              style={{
+                position: "absolute",
+                color: "white",
+                bottom: 15,
+                left: 20,
+              }}
+              name="lightning-bolt"
+              size={24}
+              color="black"
+            />
+          </Pressable>
+        ))}
+      {workouts.length === 0 && <Text>No data</Text>}
     </View>
   );
 };
